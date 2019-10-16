f42813a1c39a7a682dbf17d955384c09935420bf
brylevkirill <brylevkirill@gmail.com>
2019-10-17 01:47:46 +0300

    .

--- beauty.py
+++ beauty.py
@@ -1,21 +1,27 @@
-import random, subprocess, sys
+import random, subprocess, sys, time
 
 tmp_file="tmp.mp4"
+skip=4
+part=0.2
+offset=0.05
 
 labels = [0.0] + [float(line.split()[0]) for line in open(sys.argv[2])]
+max_interval = max([labels[i+1]-labels[i] for i in (0, len(labels)-2)]) + 1
 
 video_duration=[]
 for i in range(4, len(sys.argv)):
 	process = subprocess.run(["ffprobe", "-show_entries", "format=duration", "-v", "quiet", "-of", "csv=p=0", sys.argv[i]], stdout=subprocess.PIPE)
 	video_duration.append(float(process.stdout))
 
+random.seed(time.time())
+
 args = ["ffmpeg"]
 for i in range(0, len(labels)-1):
-	video_index=random.randint(0, len(video_duration)-1)
-	position = 10 + random.uniform(0, video_duration[video_index]-30)
-	args += ["-itsoffset", "-0.7", "-ss", str(position), "-t", str(labels[i+1]-labels[i]), "-i", sys.argv[4+video_index]]
+	video_index=(i*skip)%len(video_duration)
+	position = video_duration[video_index] * (1 - part) * i / len(labels) + random.uniform(0, video_duration[video_index] * part - max_interval)
+	duration = labels[i+1]-labels[i]-offset
+	args += ["-itsoffset", "0", "-ss", str(position), "-t", str(duration), "-i", sys.argv[4+video_index]]
 args += ["-filter_complex", "concat=n=%d" % (len(labels)-1), "-y", tmp_file]
-print(*args)
 subprocess.run(args)
 
-subprocess.run(["ffmpeg", "-i", sys.argv[1], "-itsoffset", "-1.0", "-i", tmp_file, "-c", "copy", "-y", sys.argv[3]])
+subprocess.run(["ffmpeg", "-i", sys.argv[1], "-itsoffset", "0", "-i", tmp_file, "-c", "copy", "-y", sys.argv[3]])
