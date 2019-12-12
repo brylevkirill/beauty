dde565a07937b32288c0eb5f50981d0643268c62
brylevkirill <brylevkirill@gmail.com>
2019-12-13 02:45:56 +0300

    .

--- beauty.py
+++ beauty.py
@@ -1,5 +1,6 @@
 import collections
 import datetime
+import multiprocessing.pool
 import os
 import random
 import subprocess
@@ -12,6 +13,8 @@ strategy2 = True
 strategy2_offset = -0.0245
 drop_hard_cuts = True
 drop_hard_cuts_prob = 0.05
+cache_file_name = "%d.mp4"
+temp_file_name = "tmp.mp4"
 
 Label = collections.namedtuple("Label", """
     output_start_pos
@@ -28,8 +31,10 @@ def parse_timestamp(s):
 	try:
 		return float(s)
 	except:
-		t = datetime.datetime.strptime(s, "%H:%M:%S.%f")
-		return t.hour * 3600 + t.minute * 60 + t.second + t.microsecond * 0.000001
+		pass
+	t = datetime.datetime.strptime(s, "%H:%M:%S.%f")
+	return t.hour * 3600 + t.minute * 60 + t.second + t.microsecond * 0.000001
+
 def format_timestamp(t):
 	return datetime.datetime.utcfromtimestamp(t).strftime("%H:%M:%S.%f")[:-3]
 
@@ -41,6 +46,7 @@ def parse_label(s):
 	    input_file_name = t[2] if t[2:] and os.path.isfile(t[2]) else None,
 	    input_start_pos = parse_timestamp(t[3]) if t[3:] else -1,
 	    input_end_pos = parse_timestamp(t[4]) if t[4:] else -1)
+
 def format_label(l: Label):
 	return "%s\t%s\t%s\t%s\t%s\n" % (
 	    format_timestamp(l.output_start_pos),
@@ -78,8 +84,8 @@ def read_videos():
 		    "-of", "default=noprint_wrappers=1:nokey=1",
 		    "-v", "quiet",
 		    v],
-		    check=True,
-		    stdout=subprocess.PIPE)
+		    check = True,
+		    stdout = subprocess.PIPE)
 		videos[v] = Video(duration = float(process.stdout))
 
 def handle_label(l: Label, n):
@@ -106,29 +112,40 @@ def handle_label(l: Label, n):
 	    input_start_pos,
 	    input_end_pos), label_changed
 
-def match_video(l:Label):
+def match_input_video(l: Label):
 	if drop_hard_cuts:
 		process = subprocess.run(["ffmpeg",
 		    "-ss", str(l.input_start_pos),
 		    "-t", str(l.input_end_pos - l.input_start_pos),
 		    "-i", l.input_file_name,
-		    "-filter:v" , "select='gt(scene,%f)',showinfo" % drop_hard_cuts_prob,
+		    "-filter:v", "select='gt(scene,%f)',showinfo" % drop_hard_cuts_prob,
 		    "-f", "null",
 		    "-"],
-		    check=True,
-		    stderr=subprocess.PIPE)
+		    check = True,
+		    stderr = subprocess.PIPE)
 		if "n:   0" in process.stderr.decode():
 			return False
 	return True
 
-def create_video():
+def cache_input_video(l: Label, n):
+	subprocess.run(["ffmpeg",
+	    "-ss", str(l.input_start_pos),
+	    "-t", str(l.input_end_pos - l.input_start_pos + strategy2_offset),
+	    "-i", l.input_file_name,
+	    "-filter_complex", "concat=n=1",
+	    "-an",
+	    "-y", "%d.mp4" % (n + 1)],
+	    check = True)
+
+def write_output_video():
 	read_labels()
 	read_videos()
 	strategy1_args = []
+	strategy2_pool = multiprocessing.pool.ThreadPool(os.cpu_count()//2)
 	for i in range(len(labels)):
 		while True:
 			l, label_changed = handle_label(labels[i], i)
-			if not label_changed or match_video(l):
+			if not label_changed or match_input_video(l):
 				labels[i] = l
 				break
 		if strategy1:
@@ -138,46 +155,43 @@ def create_video():
 			    "-i", l.input_file_name]
 		if strategy2 and (
 		    label_changed or not os.path.isfile("%d.mp4" % (i + 1))):
-			subprocess.run(["ffmpeg",
-			    "-ss", str(l.input_start_pos),
-			    "-t", str(l.input_end_pos - l.input_start_pos + strategy2_offset),
-			    "-i", l.input_file_name,
-			    "-filter_complex", "concat=n=1",
-			    "-an",
-			    "-y", "%d.mp4" % (i + 1)],
-			    check=True)
+			strategy2_pool.apply_async(cache_input_video, (l, i))
 	write_labels()
 	write_subtitles()
 	if strategy1:
 		subprocess.run(["ffmpeg"] + strategy1_args + [
-		     "-filter_complex", "concat=n=%d" % (len(labels)),
+		     "-filter_complex", "concat=n=%d" % len(labels),
 		     "-an",
-		     "-y", "tmp.mp4"],
-		     check=True)
+		     "-y", temp_file_name],
+		     check = True)
 	elif strategy2:
+		strategy2_pool.close()
+		strategy2_pool.join()
 		process = subprocess.Popen(["ffmpeg",
 		    "-protocol_whitelist", "file,pipe",
 		    "-f", "concat",
 		    "-safe", "0",
 		    "-i", "pipe:",
 		    "-c", "copy",
-		    "-y", "tmp.mp4"],
-		    stdin=subprocess.PIPE)
+		    "-y", temp_file_name],
+		    stdin = subprocess.PIPE)
 		process.stdin.writelines(
-		    b"file '%d.mp4'\n" % (i + 1) for i in range(len(labels)))
+		    ("file '%s'\n" % cache_file_name % (i + 1)).encode()
+		    for i in range(len(labels)))
 		process.communicate()
 		process.stdin.close()
 	if strategy1 or strategy2:
 		subprocess.run(["ffmpeg",
-		    "-i", "tmp.mp4",
+		    "-i", temp_file_name,
 		    "-i", sys.argv[1],
 		    "-c", "copy",
 		    "-y", sys.argv[3]],
-		    check=True)
+		    check = True)
+		os.remove(temp_file_name)
 
 def main():
 	random.seed(time.time())
-	create_video()
+	write_output_video()
 
 if __name__== "__main__":
 	main()
